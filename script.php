9fb688068fd9bf303dcdd96168383a9baece4d1b
Vertrexia <zodiacsohma1@gmail.com>
2014-03-10 14:10:06 +1000

    Implemented race after done
    Was able to concentrate on the point when a race finishes but players
    continue to play in it. This newly implemented method will ensure script
    will record them but will not store to the when the race is done.

--- script.php
+++ script.php
@@ -1,8 +1,9 @@
 #!/usr/bin/php
 <?php
-$map = "";          //  name of the current map
-$key = "";          //  the hash format of the current map + .txt
-$process = false;   //  flag to get the race going
+$map    = "";          //  name of the current map
+$map_id = "";          //  the hash format of the current map + .txt
+$race_work = false; //  flag to get the race going
+$race_done = false; //  the race is finally complete
 $game_time = 0;     //  the current game time
 
 $first = true;      //  flag to indicate first player entered
@@ -34,9 +35,8 @@ while (!feof(STDIN))
     {
         echo "CLEAR_LADDERLOG\n";
 
-        if (!$process) continue;
-
-        $map = $part[3];
+        //  fetch the map name from the 3rd part (if contains spaces)
+        $map = BridgeParts($part, 3);
 
         /*
          * This process is to ensure to get the MAP_FILE only and not the appended MAP_URI like in the example:
@@ -46,13 +46,18 @@ while (!feof(STDIN))
         if ($pos = strpos($map, "(") !== false)
             $map = substr($map, 0, $pos);
 
-        $key = md5($map.".txt");
-        $records[$key] = array();
+        $map_id = md5($map.".txt");
+        if ($race_work && !$race_done)
+        {
+            if (isset($records[$map_id]))
+                unset($records[$map_id]);
+            $records[$map_id] = array();
 
-        //  reset the file for fresh entries
-        $file = fopen("./data/".$key.".txt", "w+");
-        ftruncate($file, 0);
-        fclose($file);
+            //  reset the file for fresh entries
+            $file = fopen("./data/".$map_id.".txt", "w+");
+            ftruncate($file, 0);
+            fclose($file);
+        }
 
         $first = true;
         $rank = 0;
@@ -73,83 +78,82 @@ while (!feof(STDIN))
     //  CYCLE_DESTROYED [auth_name] [posx] [posy] [dirx] [diry] [team_name] [time]
     elseif ($part[0] == "CYCLE_DESTROYED")
     {
-        $key = md5($part[1]);
-        if (isset($cycles[$key]))
-            unset($cycles[$key]);
+        $p_id = md5($part[1]);
+        if (isset($cycles[$p_id]))
+            unset($cycles[$p_id]);
     }
     //  PLAYER_GRIDPOS [player_username] [pos_x] [pos_y] [dir_x] [dir_y] [cycle_speed] [player_rubber] [cycle_rubber] [team]
     elseif ($part[0] == "PLAYER_GRIDPOS")
     {
-        $key = md5($part[1]);
-        if (!isset($cycles[$key])) continue;
+        $p_id = md5($part[1]);
+        if (!isset($cycles[$p_id])) continue;
 
-        $cycles[$key][0] = floatval($part[6]);
+        $cycles[$p_id][0] = floatval($part[6]);
 
         //  do idle kill
-        if ($cycles[$key][0] <= $idle_speed)
+        if ($cycles[$p_id][0] <= $idle_speed)
         {
-            if ($cycles[$key][2] == 0)
-                $cycles[$key][2] = $game_time + $idle_delay;
+            if ($cycles[$p_id][2] == 0)
+                $cycles[$p_id][2] = $game_time + $idle_delay;
 
-            if ($game_time >= $cycles[$key][2])
+            if ($game_time >= $cycles[$p_id][2])
             {
-                if ($cycles[$key][3] < $idle_warnings)
+                if ($cycles[$p_id][3] < $idle_warnings)
                 {
                     pm($part[2], "0xff9999Idle Warning: 0xRESETTHold down your brake button (v) key to go faster.");
-                    $cycles[$key][2] = $game_time + $idle_delay;
-                    $cycles[$key][3] += 1;
+                    $cycles[$p_id][2] = $game_time + $idle_delay;
+                    $cycles[$p_id][3] += 1;
                     continue;
                 }
 
-                if ($cycles[$key][1])   //  if the player is idle
+                if ($cycles[$p_id][1])   //  if the player is idle
                 {
                     echo "KILL ".$part[1]."\n";
                     con("0xff55ff".$part[1]." 0xff9999is killed for idling around!");
-                    unset($cycles[$key]);
+                    unset($cycles[$p_id]);
                 }
                 else
                 {
-                    $cycles[$key][1] = true;
-                    $cycles[$key][2] = $game_time + $idle_delay;
+                    $cycles[$p_id][1] = true;
+                    $cycles[$p_id][2] = $game_time + $idle_delay;
                 }
             }
         }
         else
         {
-            $cycles[$key][1] = true;
-            $cycles[$key][2] = 0;
+            $cycles[$p_id][1] = true;
+            $cycles[$p_id][2] = 0;
         }
     }
     //  TARGETZONE_PLAYER_ENTER <object_id> <zone_name> <cx> <cy> <player> <x> <y> <xdir> <ydir> <time>
     //  WINZONE_PLAYER_ENTER <object id> <zone_name> <cx> <cy> <player> <x> <y> <xdir> <ydir> <time>
     elseif (($part[0] == "TARGETZONE_PLAYER_ENTER") || ($part[0] == "WINZONE_PLAYER_ENTER"))
     {
-        if (!$process) continue;
-
         $user = $part[5];
+        $p_id = md5($user);
         $time = floatval(trim($part[10]));
+        if (!isset($cycles[$p_id])) continue;
 
         $found = false;
-        if (isset($records[$key]))
+        $found_id = 0;
+        if (isset($records[$map_id]))
         {
-            for ($i = 0; $i < count($records[$key]); $i++)
+            for ($r_id = 0; $r_id < count($records[$map_id]); $r_id++)
             {
-                if ($records[$key][$i][0] == $user)
+                if ($records[$map_id][$r_id][0] == $user)
                 {
                     $found = true;
+                    $found_id = $r_id;
                     break;
                 }
             }
         }
 
-        //  skip player entry process since they already exist in the list
-        if ($found) continue;
-
         echo "KILL ".$user."\n";
+        if (isset($cycles[$p_id]))
+            unset($cycles[$p_id]);
 
         $rank++;
-        $records[$key][] = array($user, $time);
-        file_put_contents("./data/".$key.".txt", $user." ".$time."\n", FILE_APPEND);
 
         if ($first)
         {
@@ -164,15 +168,39 @@ while (!feof(STDIN))
         }
         else
             con($rank.") 0x9955ff".$user." 0xffff44finished in 0x88ff33".$time." seconds 0x92aba0(0x00aa11".($time - $first_time)." 0xffaa00seconds behind 0xff77ff".$first_player." 0x92aba0).");
+
+        if ($found)
+        {
+            if ($records[$map_id][$found_id][1] < $time)
+                pm($user, " 0x00ccffiYou are ".($time - $records[$map_id][$found_id][1])."s faster from your first race.");
+            elseif ($records[$map_id][$found_id][1] > $time)
+                pm($user, " 0x00ccffiYou are ".($time - $records[$map_id][$found_id][1])."s slower from your first race.");
+        }
+        else
+        {
+            if (!isset($records[$map_id]))
+                $records[$map_id] = array();
+
+            $records[$map_id][] = array($user, $time);
+
+            if ($race_work && !$race_done)
+                file_put_contents("./data/".$map_id.".txt", $user." ".$time."\n", FILE_APPEND);
+        }
     }
     //  INVALID_COMMAND [command] [player_username] [ip_address] [access_level] [params] ...
     elseif ($part[0] == "INVALID_COMMAND")
     {
-        if ($part[4] <= 2)  //  allow only moderators and below access levels
+        if ($part[4] <= 1)  //  allow only officers and lower level users
         {
             if ($part[1] == "/start")
             {
-                if ($process)
+                if ($race_done)
+                {
+                    pm($part[2], "The race is already complete.");
+                    continue;
+                }
+
+                if ($race_work)
                 {
                     pm($part[2], "The race is already active.");
                     continue;
@@ -181,19 +209,73 @@ while (!feof(STDIN))
                 echo "START_NEW_MATCH\n";
                 echo "KILL_ALL\n";
 
-                $process = true;
+                $race_work = true;
                 con("The race will begin from next match.");
             }
             elseif ($part[1] == "/stop")
             {
-                if (!$process)
+                if ($race_done)
+                {
+                    pm($part[2], "The race is already complete.");
+                    continue;
+                }
+
+                if (!$race_work)
                 {
                     pm($part[2], "The race is already inactive.");
                     continue;
                 }
 
-                $process = false;
+                $race_work = false;
                 con("The race will now stop.");
+            }
+            elseif ($part[1] == "/reset")
+            {
+                if ($race_done)
+                {
+                    $choice = strtolower(trim(@$part[5]));
+                    if ($choice == "")
+                    {
+                        pm($part[2], "Do you still want to reset since the race is done?");
+                        continue;
+                    }
+                    elseif (($choice == "y") || ($choice == "yes"))
+                    {
+                        pm($part[2], "Fine. The script will reset the race now.");
+                    }
+                    else
+                    {
+                        pm($part[2], "The race reset will now quit reset.");
+                        continue;
+                    }
+                }
+
+                $race_work = false;
+                $race_done = false;
+
+                $first = true;
+                $rank = 0;
+
+                $timer_active  = false;
+                $timer_counter = -1;
+
+                //  reset the lists
+                unset($cycles);
+                unset($records);
+                $cycles = array();
+                $records = array();
+
+                $files = scandir("./data");
+                if (count($files) > 0)
+                {
+                    for($f_id = 0; $f_id < count($files); $$f_id++)
+                    {
+                        if (is_file("./data/".$files[$f_id]))
+                            unlink("./data/".$files[$f_id]);
+                    }
+                }
+
+                con("The race is now reset.");
             }
         }
         else pm($part[2], 'You don not have the required access level to access "'.$part[1].'"');
@@ -232,9 +314,10 @@ while (!feof(STDIN))
     //  NEW_MATCH <date and time>
     elseif ($part[0] == "NEW_MATCH")
     {
-        if ($process)
+        if ($race_work)
         {
-            $process = false;
+            $race_work = false;
+            $race_done = true;
             con("The race has finally come to an end.");
         }
     }
@@ -253,5 +336,20 @@ function cen($msg)
 function pm($user, $msg)
 {
     echo "PLAYER_MESSAGE ".$user.' "'.addslashes($msg).'"'."\n";
+}
+
+function BridgeParts($parts, $x, $separator = " ", $end_with = "")
+{
+    $ret = "";
+
+    for(; $x < count($parts); $x++)
+    {
+        if (($x + 1) == count($parts))
+            $ret .= $parts[$x].$end_with;
+        else
+            $ret .= $parts[$x].$separator;
+    }
+
+    return $ret;
 }
 ?>
