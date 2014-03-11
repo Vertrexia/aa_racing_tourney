7ea2ddb811fa36e5754175ec66af9cd16818b9e4
Vertrexia <zodiacsohma1@gmail.com>
2014-03-11 12:14:51 +1000

    Added comments and additional codes
    I've commented on various aspects of the code which could cause
    confusion in programmers when they look at this.
    
    I have also made some modifications to some console/player messages.
    
    Added the activation and deactivation of the timer when no humans are
    alive.

--- script.php
+++ script.php
@@ -33,6 +33,7 @@ while (!feof(STDIN))
     //  CURRENT_MAP [factor] [multiplier] [MAP_FILE]
     if ($part[0] == "CURRENT_MAP")
     {
+        //  very handy to clear ladderlog since it does get big in a round
         echo "CLEAR_LADDERLOG\n";
 
         //  fetch the map name from the 3rd part (if contains spaces)
@@ -49,6 +50,7 @@ while (!feof(STDIN))
         $map_id = md5($map.".txt");
         if ($race_work && !$race_done)
         {
+            //  reset records for new entries
             if (isset($records[$map_id]))
                 unset($records[$map_id]);
             $records[$map_id] = array();
@@ -86,8 +88,11 @@ while (!feof(STDIN))
     elseif ($part[0] == "PLAYER_GRIDPOS")
     {
         $p_id = md5($part[1]);
+
+        //  do not function if the script doesn't recognise the player is alive (even if on grid)
         if (!isset($cycles[$p_id])) continue;
 
+        //  store the speed currently travelling by the user
         $cycles[$p_id][0] = floatval($part[6]);
 
         //  do idle kill
@@ -96,17 +101,21 @@ while (!feof(STDIN))
             if ($cycles[$p_id][2] == 0)
                 $cycles[$p_id][2] = $game_time + $idle_delay;
 
+            //  if the game time approached the delayed time
             if ($game_time >= $cycles[$p_id][2])
             {
+                //  if the player needs warnings
                 if ($cycles[$p_id][3] < $idle_warnings)
                 {
+                    //  send the warnings and count it up
                     pm($part[2], "0xff9999Idle Warning: 0xRESETTHold down your brake button (v) key to go faster.");
                     $cycles[$p_id][2] = $game_time + $idle_delay;
                     $cycles[$p_id][3] += 1;
                     continue;
                 }
 
-                if ($cycles[$p_id][1])   //  if the player is idle
+                //  if the player is already actively idle, kill them
+                if ($cycles[$p_id][1])
                 {
                     echo "KILL ".$part[1]."\n";
                     con("0xff55ff".$part[1]." 0xff9999is killed for idling around!");
@@ -114,6 +123,7 @@ while (!feof(STDIN))
                 }
                 else
                 {
+                    //  make them actively idle
                     $cycles[$p_id][1] = true;
                     $cycles[$p_id][2] = $game_time + $idle_delay;
                 }
@@ -121,8 +131,10 @@ while (!feof(STDIN))
         }
         else
         {
-            $cycles[$p_id][1] = true;
+            //  when they are not idle, disable the idle and warnings
+            $cycles[$p_id][1] = false;
             $cycles[$p_id][2] = 0;
+            $cycles[$p_id][3] = 0;
         }
     }
     //  TARGETZONE_PLAYER_ENTER <object_id> <zone_name> <cx> <cy> <player> <x> <y> <xdir> <ydir> <time>
@@ -132,12 +144,15 @@ while (!feof(STDIN))
         $user = $part[5];
         $p_id = md5($user);
         $time = floatval(trim($part[10]));
+
+        //  do not function if the script doesn't recognise the player is alive (even if on grid)
         if (!isset($cycles[$p_id])) continue;
 
         $found = false;
         $found_id = 0;
         if (isset($records[$map_id]))
         {
+            //  check for user existence in the records and their id
             for ($r_id = 0; $r_id < count($records[$map_id]); $r_id++)
             {
                 if ($records[$map_id][$r_id][0] == $user)
@@ -149,12 +164,15 @@ while (!feof(STDIN))
             }
         }
 
+        //  kill the user since they finished the race
         echo "KILL ".$user."\n";
         if (isset($cycles[$p_id]))
             unset($cycles[$p_id]);
 
+        //  increase the rank of entry. Default: 0. So increase by 1 each time.
         $rank++;
 
+        //  if the user is the first to finish
         if ($first)
         {
             con($rank.") 0x9955ff".$user." 0xffff44finished in 0x88ff33".$time." seconds0xffff44.");
@@ -166,23 +184,32 @@ while (!feof(STDIN))
             //  start the timer
             $timer_active = true;
         }
+        //  if the user is to finish after the FIRST player has
         else
             con($rank.") 0x9955ff".$user." 0xffff44finished in 0x88ff33".$time." seconds 0x92aba0(0x00aa11".($time - $first_time)." 0xffaa00seconds behind 0xff77ff".$first_player." 0x92aba0).");
 
+        //  if the player already exists within the records
         if ($found)
         {
+            //  if their current time is better than their first race
             if ($records[$map_id][$found_id][1] < $time)
+                //  tell the player how much faster they were
                 pm($user, " 0x00ccffiYou are ".($time - $records[$map_id][$found_id][1])."s faster from your first race.");
+            //  if their current time is worse than their first race
             elseif ($records[$map_id][$found_id][1] > $time)
+                //  tell the player how much slower they were
                 pm($user, " 0x00ccffiYou are ".($time - $records[$map_id][$found_id][1])."s slower from your first race.");
         }
         else
         {
+            //  if the records don't exist yet, create it!
             if (!isset($records[$map_id]))
                 $records[$map_id] = array();
 
+            //  add the player's user and time into the map record (finished based)
             $records[$map_id][] = array($user, $time);
 
+            //  if the race is working and not done yet, append their name and time in the file
             if ($race_work && !$race_done)
                 file_put_contents("./data/".$map_id.".txt", $user." ".$time."\n", FILE_APPEND);
         }
@@ -192,11 +219,12 @@ while (!feof(STDIN))
     {
         if ($part[4] <= 1)  //  allow only officers and lower level users
         {
+            //  Let's start the race if it isn't done
             if ($part[1] == "/start")
             {
                 if ($race_done)
                 {
-                    pm($part[2], "The race is already complete.");
+                    pm($part[2], "Can't start the race when it's done.");
                     continue;
                 }
 
@@ -210,13 +238,15 @@ while (!feof(STDIN))
                 echo "KILL_ALL\n";
 
                 $race_work = true;
+                $race_done = false;
                 con("The race will begin from next match.");
             }
+            //  Let's stop the race if it isn't done
             elseif ($part[1] == "/stop")
             {
                 if ($race_done)
                 {
-                    pm($part[2], "The race is already complete.");
+                    pm($part[2], "Can't stop the race when it's done.");
                     continue;
                 }
 
@@ -227,8 +257,10 @@ while (!feof(STDIN))
                 }
 
                 $race_work = false;
+                $race_done = false;
                 con("The race will now stop.");
             }
+            //  Let's reset the race for fresh start
             elseif ($part[1] == "/reset")
             {
                 if ($race_done)
@@ -245,7 +277,7 @@ while (!feof(STDIN))
                     }
                     else
                     {
-                        pm($part[2], "The race reset will now quit reset.");
+                        pm($part[2], "The race will not reset.");
                         continue;
                     }
                 }
@@ -265,6 +297,7 @@ while (!feof(STDIN))
                 $cycles = array();
                 $records = array();
 
+                //  remove all the possible files created in the data directory
                 $files = scandir("./data");
                 if (count($files) > 0)
                 {
@@ -288,8 +321,12 @@ while (!feof(STDIN))
         $humans_alive = intval($part[3]);
         $ais_alive    = intval($part[4]);
 
+        //  if only one human is alive, then activate the timer
         if (($humans_alive == 1) && !$timer_active)
             $timer_active = true;
+        //  if all humans are dead, deactivate the timer
+        elseif (($humans_alive == 0) && $timer_active)
+            $timer_active = false;
     }
     //  GAME_TIME <time> (see also: GAME_TIME_INTERVAL)
     elseif ($part[0] == "GAME_TIME")
